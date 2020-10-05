98dcbfff212164aeefab18ff4c4bcf635b1e9379
Norbert Racz <Norbert.Racz@partner.bmw.de>
2020-10-05 10:41:54 +0200

    Created task class

--- src/test/java/TaskrTest.java
+++ src/test/java/TaskrTest.java
@@ -1,9 +1,11 @@
 import org.junit.jupiter.api.Test;
+import taskr.Task;
 import taskr.Taskr;
 
 public class TaskrTest {
     @Test
     public void test() {
-        Taskr task = new Taskr();
+        Taskr taskr = new Taskr();
+        Task task = new Task();
     }
 }
