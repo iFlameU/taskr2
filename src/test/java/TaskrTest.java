75124ff619e5032569a42774165d393422b71ff5
Norbert Racz <Norbert.Racz@partner.bmw.de>
2020-10-05 10:35:07 +0200

    Added encoding and simple test

---
+++ src/test/java/TaskrTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TaskrTest {
+    @Test
+    public void test() {
+        Assertions.assertTrue(1 > 0);
+    }
+}
